cf53333a9275f7fc97698f02698dfdf223b0d6a7
Joe Fiorini <joe@joefiorini.com>
2013-05-09 10:15:30 -0400

    Update helper

--- app/helpers/half_pipe_helper.rb
+++ app/helpers/half_pipe_helper.rb
@@ -1,7 +1,7 @@
 module HalfPipeHelper
 
   def requirejs_include_tag(script, options={})
-    root = if Rails.env.production? then "client" else "components/requirejs" end
+    root = if Rails.env.production? then "scripts" else "components/requirejs" end
     javascript_include_tag "/#{root}/require.js", { data: { main: script } }
   end
 
