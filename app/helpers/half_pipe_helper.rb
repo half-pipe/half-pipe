27b18f0f4a97c5dcba14bc8962344b8ba53c9ea2
Joe Fiorini <joe@joefiorini.com>
2013-07-19 15:51:30 -0400

    Configure half_pipe for serving assets

--- app/helpers/half_pipe_helper.rb
+++ app/helpers/half_pipe_helper.rb
@@ -1,7 +1,7 @@
 module HalfPipeHelper
 
   def requirejs_include_tag(script, options={})
-    root = if Rails.env.production? then "scripts" else "components/requirejs" end
+    root = if Rails.application.config.half_pipe.serve_assets then "components/requirejs" else "scripts" end
     javascript_include_tag "/#{root}/require.js", { data: { main: script } }
   end
 
--- lib/half-pipe/rails.rb
+++ lib/half-pipe/rails.rb
@@ -1,4 +1,14 @@
 module HalfPipe
   class Engine < Rails::Engine
+
+    config.before_configuration do
+      config.half_pipe = ActiveSupport::OrderedOptions.new
+      config.half_pipe.serve_assets = false
+    end
+
+    initializer "half_pipe.middleware", after: "build_middleware_stack" do |app|
+      app.config.middleware.use Rack::HalfPipe if config.half_pipe.serve_assets
+    end
+
   end
 end
