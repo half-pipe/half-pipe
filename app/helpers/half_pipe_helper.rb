b9256d60286e6b28dc890cf5d46bedcc754706b6
Joe Fiorini <joe@joefiorini.com>
2013-07-25 10:31:27 -0400

    Fix require.js data-main path

--- app/helpers/half_pipe_helper.rb
+++ app/helpers/half_pipe_helper.rb
@@ -1,8 +1,12 @@
 module HalfPipeHelper
 
   def requirejs_include_tag(script, options={})
-    root = if Rails.application.config.half_pipe.serve_assets then "components/requirejs" else "scripts" end
-    javascript_include_tag "/#{root}/require.js", { data: { main: script } }
+    script, options = if Rails.application.config.half_pipe.serve_assets
+               ["/components/requirejs/require.js", { data: { main: "/scripts/application.js" } }]
+             else
+               ["/scripts/application.js", {}]
+             end
+    javascript_include_tag script, options
   end
 
 end
