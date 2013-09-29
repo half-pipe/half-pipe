414b11904f1c23718f5db33a58ea2e1052b7feac
Joe Fiorini <joe@joefiorini.com>
2013-09-29 14:58:43 -0400

    Remove rails example

--- examples/rails/app/controllers/posts_controller.rb
+++
@@ -1,9 +0,0 @@
-class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-end
--- examples/rails/config/initializers/sass.rb
+++
@@ -1,2 +0,0 @@
-require 'sass/importers/bower_importer'
-Sass.load_paths << Sass::Importers::BowerImporter.new("components")
