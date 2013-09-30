26203fc1498840ad2e325211adc919538fc38183
Joe Fiorini <joe@joefiorini.com>
2013-09-29 23:27:24 -0400

    Remove obsolete railtie

--- lib/generators/half_pipe/templates/railties.rb
+++ lib/generators/half_pipe/templates/railties.rb
@@ -2,6 +2,5 @@ require "active_record/railtie"
 # require "sprockets/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
 require "rails/test_unit/railtie"
 
