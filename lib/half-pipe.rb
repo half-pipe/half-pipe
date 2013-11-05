51bec01a0522542e84f46d76d03b3131a2482629
Elliott Mason <sireleo@gmail.com>
2013-11-05 11:39:33 -0500

    defers to Rails asset helpers by overriding default asset directories

--- lib/half-pipe.rb
+++ lib/half-pipe.rb
@@ -2,6 +2,12 @@ require "half-pipe/version"
 
 if defined?(Rails)
   require "half-pipe/rails"
+
+  ActionView::Helpers::AssetUrlHelper::ASSET_PUBLIC_DIRECTORIES.tap do |dirs|
+    dirs[:image]      = '/assets/images'
+    dirs[:javascript] = '/assets/scripts'
+    dirs[:stylesheet] = '/assets/styles'
+  end
 end
 
 require "rack/half-pipe"
